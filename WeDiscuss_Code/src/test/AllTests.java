09dca4ace3a38bc38499cd3cd80bd37b9bfe99e2
Salvador Alvarez Zapien <98060042+SalvadorAZ23@users.noreply.github.com>
2024-12-02 15:28:00 -0800

    Updated AllTests.java
    Included MessageTester.class in @SelectClasses

--- WeDiscuss_Code/src/test/AllTests.java
+++ WeDiscuss_Code/src/test/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ChatroomTester.class, MessageCreatorTester.class /* add more classes here */})
+@SelectClasses({ChatroomTester.class, MessageCreatorTester.class, MessageTester.class /* add more classes here */})
 class AllTests {
 	// runs all tests, remains empty
 }
